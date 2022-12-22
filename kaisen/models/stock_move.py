93274a21fc38cac2aa0b53023a582008200795a3
Vlad Anisov <v.anisov@icode.by>
2022-12-22 10:32:17 +0300

    [IMP] kaisen: KSN-6 Added product_packaging_qty field and check when switching stock picking to done
    [DOC] kaisen: KSN-6 Added docs

---
+++ kaisen/models/stock_move.py
@@ -0,0 +1,19 @@
+from odoo import models, fields
+from odoo.tools.float_utils import float_round
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    product_packaging_qty = fields.Float(string="Packaging Quantity", compute="_compute_product_packaging_qty")
+
+    def _compute_product_packaging_qty(self):
+        """Computes product_packaging_qty by qty in packaging and product qty"""
+        for record_id in self:
+            if record_id.product_packaging_id.qty:
+                packaging_uom_id = record_id.product_packaging_id.product_uom_id
+                record_id.product_packaging_qty = float_round(
+                    record_id.product_qty / record_id.product_packaging_id.qty,
+                    precision_rounding=packaging_uom_id.rounding)
+            else:
+                record_id.product_packaging_qty = 0
