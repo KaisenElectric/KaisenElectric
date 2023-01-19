c984262fd193520adb7d5af83bf455700b71dc04
Vlad Anisov <v.anisov@icode.by>
2023-01-19 09:56:46 +0300

    KSN-29

--- kaisen/models/stock_move.py
+++ kaisen/models/stock_move.py
@@ -142,30 +142,30 @@ class StockMove(models.Model):
     def _compute_product_packaging_qty(self):
         """Computes product_packaging_qty by qty in packaging and product qty"""
         for record_id in self:
+            product_packaging_qty = 0
             if record_id.product_packaging_id.qty:
                 packaging_uom_id = record_id.product_packaging_id.product_uom_id
                 if record_id.picking_id.picking_type_id.code == "incoming" and record_id.quantity_done:
-                    record_id.product_packaging_qty = float_round(
+                    product_packaging_qty = float_round(
                         record_id.quantity_done / record_id.product_packaging_id.qty,
                         precision_rounding=packaging_uom_id.rounding,
                     )
                 elif record_id.picking_id.picking_type_id.code == "outgoing" and record_id.forecast_availability:
-                    record_id.product_packaging_qty = float_round(
+                    product_packaging_qty = float_round(
                         record_id.forecast_availability / record_id.product_packaging_id.qty,
                         precision_rounding=packaging_uom_id.rounding,
                     )
                 elif record_id.picking_id.picking_type_id.code == "outgoing" and record_id.quantity_done:
-                    record_id.product_packaging_qty = float_round(
+                    product_packaging_qty = float_round(
                         record_id.quantity_done / record_id.product_packaging_id.qty,
                         precision_rounding=packaging_uom_id.rounding,
                     )
                 else:
-                    record_id.product_packaging_qty = float_round(
+                    product_packaging_qty = float_round(
                         record_id.product_qty / record_id.product_packaging_id.qty,
                         precision_rounding=packaging_uom_id.rounding,
                     )
-            else:
-                record_id.product_packaging_qty = 0
+            record_id.product_packaging_qty = abs(product_packaging_qty)
 
     def get_logismart_product_code(self):
         """Returns logismart_product_code by product and packaging"""
