aefdb1d7c1cb035e69063406bd2437aa095df1c4
Копелев Никита <n.kopelev@icode.by>
2023-03-08 16:22:18 +0300

    [IMP] kaisen: KSN-65 Added new field and logic for passing WH from PO to SO.
    [IMP] kaisen: KSN-65.
    
    [IMP] kaisen: KSN-65.
    
    [IMP] kaisen: KSN-65.

--- kaisen/models/purchase_order.py
+++ kaisen/models/purchase_order.py
@@ -4,6 +4,48 @@ from odoo import models, fields, api
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
+    intercompany_warehouse_id = fields.Many2one(
+        comodel_name="stock.warehouse",
+        string="Intercompany Warehouse",
+    )
+    is_same_partner = fields.Boolean(
+        compute="_compute_is_same_partner",
+        string="Same partner",
+        invisible=True
+    )
+    
+    @api.onchange("partner_id")
+    def _onchange_partner_id(self):
+        """
+        Method adds domain for field intercompany_warehouse_id depends on partner_id.
+        """
+        for record in self:
+            domain = [("partner_id", "=", record.partner_id.id)]
+            return {
+                "domain": {"intercompany_warehouse_id": domain},
+            }
+
+    @api.depends("partner_id")
+    def _compute_is_same_partner(self):
+        """
+        Method checks if partner_id in company_ids.partner_id.
+        """
+        for record in self:
+            company_count = self.env["res.company"].search_count([
+                "|", ("partner_id", "=", record.partner_id.id),
+                ("parent_id.partner_id", "=", record.partner_id.id)
+            ])
+            record.is_same_partner = bool(company_count)
+
+    def _prepare_sale_order_data(self, name, partner, company, direct_delivery_address):
+        """
+        Method adds intercompany_warehouse_id in SO.
+        """
+        self.ensure_one()
+        result = super()._prepare_sale_order_data(name, partner, company, direct_delivery_address)
+        result["warehouse_id"] = self.intercompany_warehouse_id.id
+        return result
+
     @api.model
     def _prepare_sale_order_line_data(self, line, company):
         """
