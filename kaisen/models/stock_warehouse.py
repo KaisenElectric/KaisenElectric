07c6c4c7dc4dd25e32982edc55fbba978b48a3a2
Копелев Никита <n.kopelev@icode.by>
2023-03-17 20:37:45 +0300

    [FIX] kaisen: KSN-61 Fixed problem with non-displaying records in "Search More..." popup.

--- kaisen/models/stock_warehouse.py
+++ kaisen/models/stock_warehouse.py
@@ -13,3 +13,13 @@ class StockWarehouse(models.Model):
         if self.env.context.get("allow_wh"):
             self = self.sudo()
         return super().name_search(name=name, args=args, operator=operator, limit=limit)
+
+    @api.model
+    def search_read(self, domain=None, fields=None, offset=0, limit=None, order=None, **read_kwargs):
+        """
+        Override method to allow searching records even if the current user
+        does not have read access to them.
+        """
+        if self.env.context.get("allow_wh"):
+            self = self.sudo()
+        return super().search_read(domain=domain, fields=fields, offset=offset, limit=limit, order=order, **read_kwargs)
