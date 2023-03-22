bb81e35f65ba67643afb45bea93a3dffbce07d2f
Копелев Никита <n.kopelev@icode.by>
2023-03-22 20:16:14 +0300

    [IMP] kaisen: KSN-61 Added logic for changing company_recipient_bank_id depends on company_id.
    [IMP] kaisen: KSN-61.
    
    [IMP] kaisen: KSN-61.

--- kaisen/models/sale_order.py
+++ kaisen/models/sale_order.py
@@ -10,18 +10,30 @@ class SaleOrder(models.Model):
         bank_id = self.env.company.partner_id.bank_ids[:1]
         return bank_id
 
-    company_recipient_bank_id = fields.Many2one(comodel_name="res.partner.bank", string="Company Recipient Bank",
-                                                help="Bank Account Number to which the invoice will be paid.",
-                                                default=_get_default_bank, check_company=False,
-                                                domain="[('id', 'in', domain_bank_ids)]")
-    domain_bank_ids = fields.Many2many(comodel_name="res.partner.bank", string="Partner banks", compute="_compute_domain_bank_ids")
+    company_recipient_bank_id = fields.Many2one(
+        comodel_name="res.partner.bank",
+        string="Company Recipient Bank",
+        help="Bank Account Number to which the invoice will be paid.",
+        default=_get_default_bank,
+        check_company=False,
+    )
+    company_partner_id = fields.Many2one(
+        comodel_name="res.partner",
+        string="Account Holder",
+        related="company_id.partner_id",
+        store=True,
+    )
 
-    @api.depends("company_id")
-    def _compute_domain_bank_ids(self):
-        """Method compute bank_ids for company_recipient_bank_id domain"""
+    @api.onchange("company_id")
+    def _onchange_company_id(self):
+        """
+        Method change company_recipient_bank_id depends on company_id
+        """
         for record in self:
-            bank_ids = self.env["res.partner.bank"].sudo().search([("partner_id", "=", record.company_id.partner_id.id)])
-            record.domain_bank_ids = bank_ids
+            bank_id = self.company_id.partner_id.bank_ids[:1]
+            record.update({
+                "company_recipient_bank_id": bank_id.id,
+            })
 
     @api.model
     def _prepare_purchase_order_line_data(self, so_line, date_order, company):
