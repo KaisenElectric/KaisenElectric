5ab6f31657b7b6749dbae4a4aade27067812a90f
Копелев Никита <n.kopelev@icode.by>
2023-03-26 18:14:14 +0300

    [NEW] stock_no_negative: KSN-93. Added new module.
    [IMP] stock_no_negative: KSN-93.

---
+++ stock_no_negative/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    "name": "Stock Disallow Negative",
+    "version": "15.0.1.0.0",
+    "category": "Inventory",
+    "summary": "Disallow negative stock levels by default",
+    "depends": ["stock"],
+    "data": ["views/product_product_views.xml", "views/stock_location_views.xml"],
+    "installable": True,
+}
