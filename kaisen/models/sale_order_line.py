87411071ba6741159eadcbfc25125f197a395ad2
Копелев Никита <n.kopelev@icode.by>
2023-04-12 00:30:05 +0300

    [IMP] kaisen: KSN-104 Added logic to change discount and taxed in all lines with user confirmation.
    [IMP] kaisen: KSN-104 Added logic to change discount and taxed in all lines with user confirmation.
    
    [IMP] kaisen: KSN-104 Added logic to change discount and taxed in all lines with user confirmation.
    
    [IMP] kaisen: KSN-104 Added logic to change discount and taxed in all lines with user confirmation.

--- kaisen/models/sale_order_line.py
+++ kaisen/models/sale_order_line.py
@@ -1,8 +1,11 @@
-from odoo import models, api, fields
+from odoo import models, fields
 
 
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    purchase_order_line_ids = fields.One2many(comodel_name="purchase.order.line", inverse_name="sale_order_line_id",
-                                              string="Sale Order Lines")
+    purchase_order_line_ids = fields.One2many(
+        comodel_name="purchase.order.line",
+        inverse_name="sale_order_line_id",
+        string="Sale Order Lines",
+    )
