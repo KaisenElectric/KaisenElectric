3ebc77d356f92fb86462236ea145a03bd5480df8
Viktor Ivanov <v.ivanov@icode.by>
2023-04-13 12:26:44 +0300

    [IMP] kaisen: New version module

--- kaisen/__manifest__.py
+++ kaisen/__manifest__.py
@@ -5,7 +5,7 @@
     "author": "ICode",
     "website": "https://icode.by/",
     "category": "Uncategorized",
-    "version": "15.0.1.0.3",
+    "version": "15.0.1.0.4",
     "license": "Other proprietary",
     "depends": [
         "base",
