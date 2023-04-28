d87ad0bd49f6d3fdc9bc834dbf781d93d616465e
Vlad Anisov <v.anisov@icode.by>
2023-04-28 20:56:43 +0300

    [IMP] kaisen: KSN-114 Added field warehouse in account move line

--- kaisen/models/account_move_line.py
+++ kaisen/models/account_move_line.py
@@ -16,6 +16,18 @@ class AccountMoveLine(models.Model):
         compute="_compute_stock_picking_names",
         store=True,
     )
+    warehouse_id = fields.Many2one(
+        comodel_name="stock.warehouse", string="Warehouse", compute="_compute_stock_warehouse_id", store=True
+    )
+
+    @api.depends("move_id.stock_valuation_layer_ids.warehouse_id")
+    def _compute_stock_warehouse_id(self):
+        """
+        Computes warehouse for account move line by valuation layer in account move.
+        It is assumed that in one Account Move, all Valuation Layers belong to one warehouse.
+        """
+        for record_id in self:
+            record_id.warehouse_id = record_id.move_id.stock_valuation_layer_ids.mapped("warehouse_id")[:1]
 
     @api.depends("sale_line_ids", "sale_line_ids.order_id", "sale_line_ids.order_id.picking_ids")
     def _compute_stock_picking_names(self):
@@ -25,6 +37,8 @@ class AccountMoveLine(models.Model):
         for record in self:
             picking_ids = record.sale_line_ids.order_id.picking_ids.filtered(lambda p_id: p_id.state != "cancel")
             stock_picking_names = ", ".join(picking_ids.mapped("name"))
-            record.write({
-                "stock_picking_names": stock_picking_names,
-            })
+            record.write(
+                {
+                    "stock_picking_names": stock_picking_names,
+                }
+            )
