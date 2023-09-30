c8aa38ed0bb882113a0da6908a7aa6dc978daf51
Viktor Ivanov <v.ivanov@icode.by>
2023-09-30 15:48:04 +0300

    KEO-6: Add eNemer - fullfillmet

--- kaisen/__manifest__.py
+++ kaisen/__manifest__.py
@@ -5,7 +5,7 @@
     "author": "ICode",
     "website": "https://icode.by/",
     "category": "Uncategorized",
-    "version": "15.0.1.0.7",
+    "version": "15.0.1.0.8",
     "license": "Other proprietary",
     "depends": [
         "base",
