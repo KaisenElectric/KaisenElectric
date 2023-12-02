6f7ff9fdcaf1d78e44baaf2ab5a40d751f99f90a
Viktor Ivanov <v.ivanov@icode.by>
2023-12-02 13:56:08 +0300

    KEO-8: Change company_recipient_bank_id when company_id was changed

--- kaisen_recipient_bank/models/sale_order.py
+++ kaisen_recipient_bank/models/sale_order.py
@@ -5,9 +5,9 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
 
-    @api.onchange("partner_id")
+    @api.onchange("partner_id", "company_id")
     def _onchange_partner_id(self):
-        recipient_bank_id = self.partner_id.property_recipient_bank_id \
+        recipient_bank_id = self.with_company(self.company_id.id).partner_id.property_recipient_bank_id \
                             or self.company_id.partner_id.bank_ids[:1]
         if recipient_bank_id and self.company_recipient_bank_id != recipient_bank_id:
             self.company_recipient_bank_id = recipient_bank_id
