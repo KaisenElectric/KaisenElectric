500ae06f188ccf1d2e5c81e264410a0ef5475931
Viktor Ivanov <ivikspb@gmail.com>
2024-02-25 13:08:00 +0300

    KEO-26: Add partner to Expenses

---
+++ kaisen_hr_expense/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Kaisen Expenses Extension",
+    "summary": """Expenses Extension""",
+    "description": """Extensions:
+    - Added partner_id field to connect partner with expenses
+    """,
+    "author": "Kaisen",
+    'category': 'Human Resources/Expenses',
+    "version": "15.0.0.1",
+    "license": "LGPL-3",
+    "depends": [
+        "hr_expense",
+    ],
+    "data": [
+        "views/hr_expense_views.xml",
+    ],
+    "qweb": [],
+    "application": True,
+}
